62ccc0dc2b0796a6975db0b698a0a21e53033d04
vkshipilov <vkshipilov@gmail.com>
2017-04-01 03:32:47 +0700

    End

--- ConsoleApplication1/Punct_stream.cpp
+++ ConsoleApplication1/Punct_stream.cpp
@@ -2,14 +2,33 @@
 
 bool Punct_stream::is_whitespace(char c)
 {
+	for (char w : white) if (c == w) return true;
 	return false;
 }
 
 Punct_stream & Punct_stream::operator>>(string & s)
 {
-	// TODO: insert return statement here
+	while (!(buffer >> s))
+	{
+		if (buffer.bad() || !source.good()) return *this;
+		buffer.clear();
+
+		string line;
+		getline(source, line);
+
+		for (char& ch : line)
+		{
+			if (is_whitespace(ch))
+				ch = ' ';
+			else if (!sensitive)
+				ch = tolower(ch);
+		}
+		buffer.str(line);
+	}
+	return *this;
 }
 
 Punct_stream::operator bool()
 {
+	return !(source.fail() || source.bad()) && source.good();
 }
--- ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/Source.cpp
@@ -1,6 +1,23 @@
 #include "Punct_stream.h"
+#include <vector>
+#include <algorithm>
+
+using namespace std;
 
 int main()
 {
+	Punct_stream ps{ cin };
+	ps.whitespace(";:,.?!(){}[]<>");
+	ps.case_sensitive(false);
 
+	cout << "Enter words:\n";
+	vector<string> vs;
+	for (string word; ps >> word;) vs.push_back(word);
+	sort(vs.begin(), vs.end());
+
+	for (int i = 0; i < vs.size(); ++i)
+	{
+		if (i == 0 || vs[i] != vs[i - 1]) cout << vs[i] << '\n';
+	}
+	return 0;
 }
